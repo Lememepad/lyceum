a481372b793cc490ebf6fd38c837fa199dd0a807
Lememepad <160495800+Lememepad@users.noreply.github.com>
2024-02-22 20:06:53 +0300

    Update settings.py

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -37,9 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'about',
-    'homepage',
-    'catalog',
+    'about.apps.AboutConfig',
+    'catalog.apps.CatalogConfig',
+    'homepage.apps.HomepageConfig',
 
 
 ]
