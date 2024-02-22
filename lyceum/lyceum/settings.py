ab9ad9721d4926e4b780c791a47918c12dd050a1
Lememepad <160495800+Lememepad@users.noreply.github.com>
2024-02-22 20:08:09 +0300

    Update settings.py

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z^%($+(_ka8bs!=qy381s&pw@r5m+r76alhq0o+@*6x9916+4h'
+SECRET_KEY = 'isnop9zdhujQAWS-hnikm0jvSG-]nik0p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
